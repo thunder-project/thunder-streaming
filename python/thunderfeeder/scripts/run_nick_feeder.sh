71a19a6cfd42a69204b596b8a7773ffdf640161c
industrial-sloth <industrial-sloth@users.noreply.github.com>
2015-01-16 16:18:06 -0500

    update nick script params

--- python/thunderfeeder/scripts/run_nick_feeder.sh
+++ python/thunderfeeder/scripts/run_nick_feeder.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-IMAGING_INPUT_DIR=/groups/freeman/freemanlab/Streaming/demo_2015_01_12/registered_im
-BEHAV_INPUT_DIR=/groups/freeman/freemanlab/Streaming/demo_2015_01_12/registered_bv
+IMAGING_INPUT_DIR=/groups/freeman/freemanlab/Streaming/demo_2015_01_16/registered_im
+BEHAV_INPUT_DIR=/groups/freeman/freemanlab/Streaming/demo_2015_01_16/registered_bv
 SPARK_OUTPUT_DIR=/nobackup/freeman/streaminginput/
 # TMP_OUTPUT_DIR must be on the same filesystem as SPARK_OUTPUT_DIR:
 TMP_OUTPUT_DIR=/nobackup/freeman/streamingtmp/
 THUNDER_STREAMING_DIR=/groups/freeman/home/swisherj/thunder-streaming
-MAX_FILES=20
+MAX_FILES=40
 
 # local testing directories - leave commented out for use on cluster:
 # IMAGING_INPUT_DIR=/mnt/data/data/from_nick/demo_2015_01_09_subset/registered_im/
@@ -19,6 +19,8 @@ MAX_FILES=20
 export TMP=$TMP_OUTPUT_DIR
 rm $SPARK_OUTPUT_DIR/*
 
+umask 000
+
 $THUNDER_STREAMING_DIR/python/thunderfeeder/grouping_series_stream_feeder.py \
 $IMAGING_INPUT_DIR  $BEHAV_INPUT_DIR  $SPARK_OUTPUT_DIR \
---max-files $MAX_FILES  --imgprefix images --behavprefix behaviour
+--max-files $MAX_FILES  --imgprefix images --behavprefix behaviour -l 60.0
