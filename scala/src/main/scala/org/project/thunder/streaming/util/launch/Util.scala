22a3b310368cb8b1ac9baa237da6ff38989bf1c8
Andrew Osheroff <Andrew@Andrews-MacBook-Air.local>
2015-02-12 19:10:14 -0500

    Removed unnecessary code from ...util/launch/Util.scala

--- scala/src/main/scala/org/project/thunder/streaming/util/launch/Util.scala
+++ scala/src/main/scala/org/project/thunder/streaming/util/launch/Util.scala
@@ -8,35 +8,6 @@ import scala.xml.{Node, NodeSeq}
 object Util {
 
   /**
-   * Given a node, finds the first node with the given element name, ensure that it doesn't have any children, and
-   * extracts its text
-   *
-   * @param node The parent node (NodeSeq for compatibility with Scala's XML API)
-   * @param nodeName The node whose text is being extracted
-   * @return The extracted text, if found
-   */
-  def getFirstNodeText(node: NodeSeq, nodeName: String): Option[String] = {
-    val matchingNodes = node \ nodeName
-    if (matchingNodes.length > 0) {
-      val firstNode = matchingNodes(0)
-      if firstNode.child.length == 1
-    }
-  }
-
-  /**
-   * Given a node, checks whether that node has any child elements, or is a leaf (surprisingly couldn't find this in
-   * the XML API
-   *
-   * TODO is there a more idiomatic Scala way of doing this?
-   *
-   * @param node The parent node (NodeSeq for compatibility with Scala's XML API)
-   * @return Boolean saying whether the node is a leaf
-   */
-  def isLeafNode(node: NodeSeq): Boolean = {
-    false
-  }
-
-  /**
    * Constructs an object of class T by calling its constructor with arguments args. The resulting object is properly
    * typed.
    *
