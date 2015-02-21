b82cf0f8072f7bcf8bc91c2916dafc31a3708bf8
freeman <the.freeman.lab@gmail.com>
2015-02-21 14:42:40 -0500

    Get key automatically from index

--- scala/src/main/scala/org/project/thunder/streaming/rdds/StreamingSeriesLoader.scala
+++ scala/src/main/scala/org/project/thunder/streaming/rdds/StreamingSeriesLoader.scala
@@ -20,7 +20,9 @@ class StreamingSeriesLoader(ssc: StreamingContext) {
   def fromBinary(dir: String, valueType: String): StreamingSeries = {
     val parser = new Parser(valueType)
     val lines = ssc.fileStream[LongWritable, BytesWritable, FixedLengthBinaryInputFormat](dir)
-    val dstream = lines.map{ case (k, v) => v.getBytes}.map(parser.getWithKey)
+    val dstream = lines
+      .map{ case (k, v) => (k.get().toInt, v.getBytes)}
+      .map{ case (k, v) => (k, parser.get(v))}
     new StreamingSeries(dstream)
   }
 
