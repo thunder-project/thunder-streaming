f864dc616f6c54380f3d8e028af85c51224fcfc1
Osheroff <osheroffa@freemanj-wm4.hhmi.org>
2015-02-23 18:32:27 -0500

    First commit of the SeriesTestAnalysis class, which contains a set of simple StreamingSeries analyses which can be used for testing. Also implementing the count() function on StreamingSeries

---
+++ scala/src/main/scala/org/project/thunder/streaming/analyses/CountingAnalysis.scala
@@ -0,0 +1,8 @@
+package org.project.thunder.streaming.analyses
+
+/**
+ * Created by osheroffa on 2/23/15.
+ */
+class CountingAnalysis extends   {
+
+}
---
+++ scala/src/main/scala/org/project/thunder/streaming/analyses/SeriesTestAnalysis.scala
@@ -0,0 +1,47 @@
+package org.project.thunder.streaming.analyses
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.project.thunder.streaming.analyses.Analysis.OutputListType
+import org.project.thunder.streaming.rdds.StreamingSeries
+import org.project.thunder.streaming.util.ThunderStreamingContext
+
+import scala.util.{Failure, Success, Try}
+
+abstract class SeriesTestAnalysis(tssc: ThunderStreamingContext, params: Map[String, String]) extends Analysis[Int, Array[Double]] {
+
+  def getSeries(): StreamingSeries = {
+    val dataPath = params.getOrElse(SeriesTestAnalysis.DATA_PATH_KEY, "series_data")
+    tssc.loadStreamingSeries(dataPath, inputFormat = params.getOrElse(SeriesTestAnalysis.FORMAT_KEY, ""))
+  }
+
+  def register(outputs: OutputListType): Unit = {
+    val data = getSeries()
+    val outputFuncs = getOutputFunctions(outputs)
+    val analyzedData  = analyze(data)
+    analyzedData.outputWithKeys(outputFuncs)
+  }
+
+  def analyze(data: StreamingSeries): StreamingSeries
+
+}
+
+object SeriesTestAnalysis {
+  final val DATA_PATH_KEY = "data_path"
+  final val FORMAT_KEY = "format"
+}
+
+class SeriesMeanAnalysis(tssc: ThunderStreamingContext, params: Map[String, String]) extends SeriesTestAnalysis(tssc, params) {
+  def analyze(data: StreamingSeries): StreamingSeries = {
+    data.seriesMean()
+  }
+}
+
+class SeriesCountAnalysis(tssc: ThunderStreamingContext, params: Map[String, String]) extends SeriesTestAnalysis(tssc, params) {
+  def analyze(data: StreamingSeries): StreamingSeries = {
+    data.count()
+  }
+}
+
+
+
