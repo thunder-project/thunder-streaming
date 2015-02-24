7234d778fb658d1f5c055ff53654441086caee23
Andrew Osheroff <Andrew@Andrews-MacBook-Air.local>
2015-02-24 11:11:42 -0500

    Updated the sample script

--- python/examples/sample_script.py
+++ python/examples/sample_script.py
@@ -3,10 +3,13 @@ source_path = "/Users/Andrew/Work/development/sample_data/data/"
 output_path = "/Users/Andrew/Work/development/sample_data/series_output/"
 checkpoint_dir = "/Users/Andrew/Work/development/sample_data/checkpoint/"
 
-analysis = Analysis.SimpleExampleAnalysis(data_path=source_path, format="text")
+analysis1 = Analysis.SeriesMeanAnalysis(data_path=source_path, format="text")
 output1 = Output.SeriesFileOutput(directory=output_path, prefix="output", format="text",
         include_keys="true")
 
-tsc.add_analysis(analysis, output1)
+analysis1.add_output(output1)
+
+tsc.add_analysis(analysis)
+
 tsc.set_checkpoint_dir(checkpoint_dir)
 
