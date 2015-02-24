cfaeab3e6ab0c3e4ec48ffe932b13fab6ca80ac8
Andrew Osheroff <Andrew@Andrews-MacBook-Air.local>
2015-02-24 17:15:05 -0500

    Made sample_analysis.py simpler to use and removed unused samples

--- python/examples/sample_analysis.py
+++ python/examples/sample_analysis.py
@@ -1,12 +1,22 @@
-tsc.set_checkpoint_dir("/Users/osheroffa/Work/sample_data/checkpoint/")
 
-analysis1 = Analysis.SimpleExampleAnalysis(data_path="/Users/osheroffa/Work/development/sample_data/data", format="text")
+# Replace my strings with yours
+CHECKPOINT_DIR = "/Users/osheroffa/Work/sample_data/checkpoint/"
+READ_LOCATION = "/Users/osheroffa/Work/development/sample_data/data"
+WRITE_LOCATION = "/Users/osheroffa/Work/development/sample_data/series_output/"
+MASTER = "local[20]"
 
-output1 = Output.SeriesFileOutput(directory="/Users/osheroffa/Work/development/sample_data/series_output/", prefix="output", format="text", include_keys="true")
-output2 = Output.SeriesFileOutput(directory="/Users/osheroffa/Work/development/sample_data/series_output/",prefix="output", format="binary", include_keys="true")
+# This script creates one SeriesNoopAnalysis (which just sends the unmodified input to any 
+# outputs) and write the output to a file using a SeriesFileOutput.
 
-analysis1.add_output(output1, output2)
+tsc.set_checkpoint_dir(CHECKPOINT_DIR)
+
+analysis1 = Analysis.SeriesNoopAnalysis(data_path=READ_LOCATION, format="text")
+
+output1 = Output.SeriesFileOutput(directory=WRITE_LOCATION, prefix="output", format="text", include_keys="true")
+
+analysis1.add_output(output1)
 
 tsc.add_analysis(analysis1)
-tsc.set_master("local[20]")
+tsc.set_master(MASTER)
+
 tsc.start()
--- python/examples/sample_script2.py
+++
@@ -1,4 +0,0 @@
-source_path = "/Users/Andrew/Work/development/sample_data/binary_input/"
-analysis = Analysis.SimpleExampleAnalysis(data_path=source_path, format="binary")
-tsc.add_analysis(analysis, output1)
-
