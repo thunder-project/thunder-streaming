e5a7fe7276afcc8ba3de42b317946020ae01f540
industrial-sloth <industrial-sloth@noreply.github.com>
2015-02-24 12:44:08 -0500

    test casting keys

--- scala/src/main/scala/org/project/thunder/streaming/rdds/StreamingSeriesLoader.scala
+++ scala/src/main/scala/org/project/thunder/streaming/rdds/StreamingSeriesLoader.scala
@@ -20,7 +20,7 @@ class StreamingSeriesLoader(ssc: StreamingContext) {
   def fromBinary(dir: String, valueType: String): StreamingSeries = {
     val parser = new Parser(valueType)
     val lines = ssc.fileStream[LongWritable, BytesWritable, FixedLengthBinaryInputFormat](dir)
-    val dstream = lines.map{ case (k, v) => v.getBytes}.map(parser.getWithKey)
+    val dstream = lines.map{ case (k, v) => (k.get().toInt, parser.get(v.getBytes))}
     new StreamingSeries(dstream)
   }
 
