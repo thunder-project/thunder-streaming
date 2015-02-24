f85f3bdfd056851904205891e4d33820335ef1e6
Andrew Osheroff <Andrew@Andrews-MacBook-Air.local>
2015-02-24 10:43:41 -0500

    Added the environment variable configuration script and two sample Python scripts which demonstrate how to use the Thunder-Streaming shell

---
+++ python/examples/sample_script2.py
@@ -0,0 +1,4 @@
+source_path = "/Users/Andrew/Work/development/sample_data/binary_input/"
+analysis = Analysis.SimpleExampleAnalysis(data_path=source_path, format="binary")
+tsc.add_analysis(analysis, output1)
+
