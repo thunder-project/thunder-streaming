5a2fb414b128a2cb82560a9c82c832d5cdb48e3a
freeman <the.freeman.lab@gmail.com>
2015-03-02 22:02:57 -0500

    Changes

--- scala/src/main/scala/org/project/thunder/streaming/examples/ExampleLoadStreaming.scala
+++ scala/src/main/scala/org/project/thunder/streaming/examples/ExampleLoadStreaming.scala
@@ -1,34 +1,29 @@
 package org.project.thunder.streaming.examples
 
-import org.apache.hadoop.io.{BytesWritable, LongWritable}
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
-import org.project.thunder.streaming.util.io.Parser
-import org.project.thunder.streaming.util.io.hadoop.FixedLengthBinaryInputFormat
+
+import org.project.thunder.streaming.util.ThunderStreamingContext
 
 object ExampleLoadStreaming {
 
   def main(args: Array[String]) {
 
-    val dataPath = args(0)
+    val master = args(0)
 
-    val batchTime = args(1).toLong
+    val dataPath = args(1)
 
-    val conf = new SparkConf()
-      .setJars(List("target/scala-2.10/thunder-streaming_2.10-0.1.0_dev.jar"))
-      .set("spark.executor.memory", "100G")
-      .set("spark.default.parallelism", "100")
-      .setAppName("ExampleLoadStreaming")
+    val batchTime = args(2).toLong
+
+    val conf = new SparkConf().setMaster(master).setAppName("ExampleLoadStreaming")
 
     val ssc = new StreamingContext(conf, Seconds(batchTime))
 
-    val parser = new Parser("short")
+    val tssc = new ThunderStreamingContext(ssc)
 
-    val lines = ssc.fileStream[LongWritable, BytesWritable, FixedLengthBinaryInputFormat](dataPath)
+    val data = tssc.loadStreamingSeries(dataPath, inputFormat="binary")
 
-    val dstream = lines.map{ case (k, v) => (k.get().toInt, parser.get(v.getBytes))}
-
-    dstream.count().print()
+    data.dstream.count().print()
 
     ssc.start()
     ssc.awaitTermination()
