97403a119286077e4a0bde26344d81e5f96e7f7e
andrewosh <andrewosh@gmail.com>
2015-03-09 16:49:20 -0400

    More progress

--- scala/src/main/scala/org/project/thunder/streaming/analyses/Analysis.scala
+++ scala/src/main/scala/org/project/thunder/streaming/analyses/Analysis.scala
@@ -15,14 +15,14 @@ object Analysis {
       <param name={name_1} value={value_1} />
       ...
       <param name={name_n} value={value_n} />
-      <output>{(see AnalysisOutput Schema )}</output>
-      <output>...</output>
     </analysis>;
    */
 
   final val OUTPUT = "output"
   final val INPUT = "input"
   final val PREFIX = "prefix"
+  final val HOST = "host"
+  final val PORT = "port"
 
   class BadAnalysisConfigException(msg: String) extends RuntimeException(msg)
 
@@ -64,9 +64,48 @@ object Analysis {
   }
 }
 
-abstract class Analysis[T <: StreamingData[_, _]](tssc: ThunderStreamingContext, params: Map[String, String]) {
+trait Updatable {
+  def handleUpdate(update: String)
+}
+
+abstract class Analysis[T <: StreamingData[_, _]](tssc: ThunderStreamingContext, params: Map[String, String])
+  extends Updatable {
 
   def getParam(key: String): String = params.getOrElse(key, "")
+
+  // Mutable parameters that can be updated at runtime by the DataReceiver Thread
+  // TODO: This should be made more generic (restricting the value type to String is too limiting)
+  var updatableParams = Map[String, String]()
+  def getUpdatableParam(key: String): String = updatableParams.getOrElse(key, "")
+  def setUpdatableParam(key: String, value: String): Unit = {
+    updatableParams = updatableParams + (key -> value)
+  }
+
+  def handleUpdate(update: String): Unit = {
+    // By default, just print the update
+    println(update)
+  }
+
+  /**
+   * Starts an UpdateListener which connects to the Python-side Analysis object and receives updates (in JSON form)
+   * from an external source. These updates must be handled in an Analysis-specific way, and will typically update
+   * the updatableVars map.
+   */
+  def startListening(): Unit = {
+    val receiverParams = Map[String, String](
+      "host" -> getParam(Analysis.HOST),
+      "port" -> getParam(Analysis.PORT)
+    )
+    val receiver = DataReceiver.getDataReceiver(this, receiverParams)
+    new Thread(new Runnable {
+      def run() = {
+        receiver.connect()
+        while (!Thread.interrupted()) {
+          receiver.receive()
+        }
+      }
+    }).start
+  }
 
   def process(): Unit = {
     val data = load(getParam(Analysis.INPUT))
---
+++ scala/src/main/scala/org/project/thunder/streaming/analyses/DataReceiver.scala
@@ -0,0 +1,34 @@
+package org.project.thunder.streaming.analyses
+
+import ak
+
+/**
+ * Receives updates from an Analysis' corresponding Python Analysis (in the manager) using a simple TCP-based
+ *
+ */
+
+abstract class DataReceiver(listener: Updatable, params: Map[String, String]) {
+  def connect(): Unit
+  def receive(): Unit
+}
+
+object DataReceiver {
+  def getDataReceiver(listener: Updatable, params: Map[String, String] = Map[String, String]()): DataReceiver = {
+    new TCPDataReceiver(listener, params)
+  }
+}
+class TCPDataReceiver(listener: Updatable, params: Map[String, String]) extends DataReceiver(listener, params) {
+
+  def connect(): Unit = {
+    val host = params.getOrElse("host", "localhost")
+    val port = params.getOrElse("port", "0").toInt
+    for {
+      conn <-
+    }
+  }
+
+  def receive(): Unit = {
+
+  }
+
+}
