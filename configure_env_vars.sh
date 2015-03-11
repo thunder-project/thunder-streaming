4499ee81286f2a5520f79a35ef7f2e905c180425
freeman <the.freeman.lab@gmail.com>
2015-03-11 13:47:49 -0400

    Tweaks

--- configure_env_vars.sh
+++ configure_env_vars.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export SPARK_HOME=~/Work/development/spark-dev/
-export THUNDER_STREAMING_PATH=~/Work/development/thunder-streaming/
+export THUNDER_STREAMING_PATH=~/github/thunder-streaming/
 export PATH=$PATH:$SPARK_HOME/bin:$THUNDER_STREAMING_PATH/python/bin/
