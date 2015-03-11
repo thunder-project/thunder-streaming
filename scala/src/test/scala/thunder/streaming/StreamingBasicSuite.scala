4c84239ff9d23193470347519b48b4d0719442c3
freeman <the.freeman.lab@gmail.com>
2015-03-11 00:28:49 -0400

    Removed non-functional test

--- scala/src/test/scala/thunder/streaming/StreamingBasicSuite.scala
+++ scala/src/test/scala/thunder/streaming/StreamingBasicSuite.scala
@@ -3,10 +3,6 @@ package thunder.streaming
 import org.apache.spark.streaming.{Milliseconds, Seconds, StreamingContext}
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.util.StatCounter
-import org.apache.spark.mllib.util.LinearDataGenerator
-import cern.colt.matrix.{DoubleFactory1D, DoubleFactory2D}
 
 import org.scalatest.FunSuite
 import com.google.common.io.Files
@@ -73,57 +69,6 @@ class StreamingBasicSuite extends FunSuite {
     assertEqual(counter(1), mean, 0.1)
     assertEqual(counter(2), variance, 0.1)
     assertEqual(counter(1), n * m, 0.0001)
-
-  }
-
-
-  test("stateful linear regression") {
-
-    // set parameters
-    val n = 10 // number of data points per record
-    val r = 0.05 // noise
-    val m = 10 // number of batches
-    val intercept = 1.0 // intercept for linear model
-    val weights = 1.5 // coefficients for linear model
-
-    // create test directory and set up streaming data
-    val testDir = Files.createTempDir()
-    val checkpointDir = Files.createTempDir()
-    val ssc = new StreamingContext(conf, Seconds(1))
-    val data = LoadStreaming.fromTextWithKeys(ssc, testDir.toString)
-
-    // create and train stateful linear regression model
-    val state = StatefulLinearRegression.trainStreaming(data, Array(0))
-    var model = new FittedModel(0.0, 0.0, 0.0, 0.0, DoubleFactory2D.dense.make(0, 0), DoubleFactory1D.dense.make(0), DoubleFactory1D.dense.make(0))
-    state.foreachRDD{rdd => model = rdd.values.first()}
-    ssc.checkpoint(checkpointDir.toString)
-
-    ssc.start()
-
-    // generate streaming data
-    val rand = new Random(41)
-
-    Thread.sleep(5000)
-    for (i <- 0 until m) {
-      val feature = Array.fill(n)(rand.nextGaussian())
-      val record = feature.map(x => x * weights + intercept + rand.nextGaussian() * r)
-      val file = new File(testDir, i.toString)
-      FileUtils.writeStringToFile(file, "0 " + feature.mkString(" ") + "\n" + "1 " + record.mkString(" ") + "\n")
-      Thread.sleep(Milliseconds(500).milliseconds)
-    }
-    Thread.sleep(Milliseconds(5000).milliseconds)
-
-    ssc.stop()
-    System.clearProperty("spark.driver.port")
-
-    FileUtils.deleteDirectory(testDir)
-    FileUtils.deleteDirectory(checkpointDir)
-
-    // compare estimated parameters to actual
-    assertEqual(model.R2, 0.99, 0.1)
-    assertEqual(model.intercept, intercept, 0.1)
-    assertEqual(model.weights, Array(weights), 0.1)
-    assertEqual(model.count, 100, 0.001)
 
   }
 
